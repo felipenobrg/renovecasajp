e3eb8e03331fe6b98ad04588175b91a02fb8939c
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-07-20 18:31:44 -0300

    adding produtos em vime

--- src/pages/Produtos-em-vime/index.tsx
+++ src/pages/Produtos-em-vime/index.tsx
@@ -1,15 +1,21 @@
 import { AboutDelivery } from "../../components/AboutDelivery";
 import { Header } from "../../components/Header";
 import { SectionLink } from "../../components/SectionLink";
-
+import BanquetaPreta from '../../assets/ProdutosEmVime/banquetaPreta.jpg'
+import BanquetaMarrom from '../../assets/ProdutosEmVime/banquetaMarrom.jpg'
+import BanquetaBege from '../../assets/ProdutosEmVime/banquetaBege.jpg'
+import { ProductCard } from "../../components/ProductCard";
 
 export function ProdutosEmVime() {
   return (
     <>
       <Header />
       <AboutDelivery />
-
       <SectionLink />
+
+      <ProductCard imgSrc={BanquetaPreta} title="Banqueta Preta em Vime" price="99,99" oldPrice="119,99" />
+      <ProductCard imgSrc={BanquetaMarrom} title="Banqueta Marrom em Vime" price="99,99" oldPrice="119,99"  />
+      <ProductCard imgSrc={BanquetaBege} title="Banqueta Bege em Vime" price="99,99" oldPrice="119,99"  />
     </>
   );
 }
