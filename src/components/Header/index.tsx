3c4cebb629b7721ce24a616dfb8dd7b0a91b3900
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-07-21 17:46:26 -0300

    deleting nav link in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,6 @@
 import { GlobalStyle } from "../../styles/global";
 import { HeaderContainer, HeaderContent } from "./styles";
 import LogoRenoveCasa from "../../assets/logoRenoveCasa.png";
-import { NavLink } from "react-router-dom";
 
 export function Header() {
   return (
@@ -9,9 +8,7 @@ export function Header() {
       <GlobalStyle />
       <HeaderContainer>
         <HeaderContent>
-          <NavLink to={"/"}>
             <img src={LogoRenoveCasa} alt="" />
-          </NavLink>
         </HeaderContent>
       </HeaderContainer>
     </>
