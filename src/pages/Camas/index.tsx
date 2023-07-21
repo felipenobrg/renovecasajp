0f49bc44e73d85eab367a494f0ca00578da4eccd
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-07-21 15:59:11 -0300

    adding cama e moveis

--- src/pages/Camas/index.tsx
+++ src/pages/Camas/index.tsx
@@ -1,7 +1,9 @@
 import { AboutDelivery } from "../../components/AboutDelivery";
 import { Header } from "../../components/Header";
+import { ProductCard } from "../../components/ProductCard";
 import { ScrollUp } from "../../components/ScrollUp";
 import { SectionLink } from "../../components/SectionLink";
+import CamaDeCasalSimples from "../../assets/Camas/CamaDeCasalSimples.jpg"
 
 
 
@@ -12,6 +14,14 @@ export function Camas() {
       <AboutDelivery />
 
       <SectionLink />
+
+      <ProductCard
+        imgSrc={CamaDeCasalSimples}
+        title="Cama Box de Casal Topázio"
+        price="549,99"
+        oldPrice="699,99"
+        textForWhatsApp="Olá. Gostaria de comprar Cama Box de Casal Topázio"
+      />
 
       <ScrollUp />
     </>
--- src/pages/Moveis/index.tsx
+++ src/pages/Moveis/index.tsx
@@ -2,6 +2,14 @@ import { AboutDelivery } from "../../components/AboutDelivery";
 import { Header } from "../../components/Header";
 import { ScrollUp } from "../../components/ScrollUp";
 import { SectionLink } from "../../components/SectionLink";
+import { ProductCard } from "../../components/ProductCard";
+
+import ArmarioPreto from '../../assets/Moveis/ArmarioDeCozinhaPreto.jpg'
+import ArmarioBranco from '../../assets/Moveis/ArmarioDeCozinhaBranco.jpg'
+import ArmarioMarrom from '../../assets/Moveis/ArmarioDeCozinhaMarrom.jpg'
+import PainelDeTv46Polegadas from '../../assets/Moveis/PainelParaTv.jpg'
+import PainelDeTv55Polegadas from '../../assets/Moveis/PainelParaTv55.jpg'
+
 
 
 
@@ -12,7 +20,41 @@ export function Moveis() {
       <AboutDelivery />
 
       <SectionLink />
-
+      <ProductCard
+        imgSrc={ArmarioPreto}
+        title="Armário de Cozinha Preto"
+        price="749,99"
+        oldPrice="899,99"
+        textForWhatsApp="Olá. Gostaria de comprar Armário de Cozinha Preto e Branco"
+      />
+       <ProductCard
+        imgSrc={ArmarioBranco}
+        title="Armário de Cozinha Preto"
+        price="749,99"
+        oldPrice="899,99"
+        textForWhatsApp="Olá. Gostaria de comprar Armário de Cozinha Preto"
+      />
+       <ProductCard
+        imgSrc={ArmarioMarrom}
+        title="Armário de Cozinha Marrom"
+        price="749,99"
+        oldPrice="899,99"
+        textForWhatsApp="Olá. Gostaria de comprar Armário de Cozinha Marrom"
+      />
+       <ProductCard
+        imgSrc={PainelDeTv46Polegadas}
+        title="Painel de TV 46 Polegadas"
+        price="449,99"
+        oldPrice="599,99"
+        textForWhatsApp="Olá. Gostaria de comprar Painel de TV 46 Polegadas"
+      />
+       <ProductCard
+        imgSrc={PainelDeTv55Polegadas}
+        title="Painel de TV 55 Polegadas"
+        price="549,99"
+        oldPrice="699,99"
+        textForWhatsApp="Olá. Gostaria de comprar Painel de TV 55 Polegadas"
+      />
       <ScrollUp />
     </>
   );
