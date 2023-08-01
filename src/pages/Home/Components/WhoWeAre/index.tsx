99a6cbb860d3d8a638d45c70c5dde8ecd88befc3
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-07-31 21:01:46 -0300

    updating

--- src/pages/Home/Components/WhoWeAre/index.tsx
+++ src/pages/Home/Components/WhoWeAre/index.tsx
@@ -2,26 +2,45 @@ import { FacebookLogo, InstagramLogo } from "phosphor-react";
 import { WhoWeAreContainer } from "./styles";
 
 export function WhoWeAre() {
-    return (
-        <WhoWeAreContainer>
-        <h1>Quem Somos?</h1>
+  return (
+    <WhoWeAreContainer>
+      <h1>Quem Somos?</h1>
+      <div>
+        <h2>Móveis de Qualidade em João Pessoa | Loja Online e Física</h2>
         <p>
-          Na Renove Casa, estamos apaixonados por tornar a sua casa um lugar
-          acolhedor, bonito e confortável, sem pesar no seu bolso. Nossa loja é
-          especializada em oferecer móveis de alta qualidade a preços
-          incrivelmente acessíveis.{" "}
+          Procurando móveis de alta qualidade a preços acessíveis em João
+          Pessoa? Você encontrou o lugar certo! Somos uma loja especializada em
+          oferecer uma ampla seleção de móveis de todos os estilos e valores,
+          trazendo conforto e estilo para o seu lar.
         </p>
+      </div>
+      <div>
+        <h2>Variedade de Estilos e Valores | Móveis com Personalidade</h2>
         <p>
-          Acreditamos que um lar bem decorado e funcional deve estar ao alcance
-          de todos. Por isso, trabalhamos diretamente com os melhores
-          fabricantes, garantindo que os produtos cheguem diretamente da fábrica
-          para a sua casa, sem intermediários.
+          Na nossa loja, você encontrará móveis para todos os gostos e
+          necessidades. Temos opções que se encaixam perfeitamente em diferentes
+          estilos de decoração, desde os mais clássicos até os mais modernos e
+          contemporâneos.
         </p>
+      </div>
+      <div>
+        <h2>Visite nossa Loja Online ou Física em João Pessoa</h2>
         <p>
-        <a href="https://www.instagram.com/renovecasajp/" ><InstagramLogo size={30} color="#929292" /></a> 
-        <a><FacebookLogo size={30}/></a>
+          Venha nos fazer uma visita em nossa loja física em João Pessoa para
+          conferir de perto a nossa seleção de móveis. Se preferir, também
+          estamos disponíveis online, facilitando suas compras a qualquer hora e
+          lugar.
         </p>
+      </div>
 
-      </WhoWeAreContainer>
-    )
+      <p>
+        <a href="https://www.instagram.com/renovecasajp/">
+          <InstagramLogo size={30} color="#929292" />
+        </a>
+        <a>
+          <FacebookLogo size={30} />
+        </a>
+      </p>
+    </WhoWeAreContainer>
+  );
 }
--- src/pages/Home/Components/WhoWeAre/styles.ts
+++ src/pages/Home/Components/WhoWeAre/styles.ts
@@ -3,18 +3,24 @@ import { styled } from "styled-components";
 export const WhoWeAreContainer = styled.div`
    display: flex;
    flex-direction: column;
+   gap: 2rem;
    margin: 2rem 2rem 1rem 2rem;
 
    h1 {
-    display: flex;
     font-family: 'Baloo 2', sans-serif;
-    justify-content: flex-start;
+    font-size: 2.2rem;
    }
 
    p {
+    margin-top: 0.5rem;
     width: 100%;
     font-size: 1rem;
-    margin-top: 1rem;
     color: ${(props => props.theme["gray-500"])};
    }
+
+   @media screen and (max-width: 768px) {
+    p {
+        font-size: 0.9rem;
+    }
+  }
 ` 
