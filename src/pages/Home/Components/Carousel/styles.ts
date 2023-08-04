e3b471deb5d51a5bab5fc8d160f7bf98d75beaf4
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-08-04 17:17:42 -0300

    updating carousel

--- src/pages/Home/Components/Carousel/styles.ts
+++ src/pages/Home/Components/Carousel/styles.ts
@@ -11,6 +11,7 @@ export const CarouselContainer = styled.div`
       display: flex;
       justify-content: center;
       align-items: center;
+      width: 100%;
       max-width: 100%;
       height: 25rem;
     }
