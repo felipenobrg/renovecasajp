1f8e6123715134933b2ba323c9e9ee839286356f
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-08-04 17:14:03 -0300

    test

--- src/pages/Home/Components/Carousel/styles.ts
+++ src/pages/Home/Components/Carousel/styles.ts
@@ -9,9 +9,8 @@ export const CarouselContainer = styled.div`
       display: flex;
       justify-content: center;
       align-items: center;
-      width: 100%;
+      max-width: 100%;
       height: 25rem;
-  display: none;
     }
   }
 
