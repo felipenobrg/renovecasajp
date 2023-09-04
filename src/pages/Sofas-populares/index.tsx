ea5ba315c082fd2b56c9dbf5ba0e0def5d7abbed
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-09-04 18:12:17 -0300

    putting search bar in sofaspopulares

--- src/pages/Sofas-populares/index.tsx
+++ src/pages/Sofas-populares/index.tsx
@@ -6,18 +6,23 @@ import { ScrollUp } from "../../components/ScrollUp";
 import { Footer } from "../../components/Footer";
 import { sofasPopularesData } from "../../data/Sofas-populares";
 import { ProductCard } from "../../components/ProductCard";
+import { SearchBar } from "../components/SearchBar";
+import { useState } from "react";
 
 export function SofasPopulares() {
+  const [searchBar, setSearchBar] = useState("");
+
+  const filteredFurnitures = sofasPopularesData.filter((product) =>
+    product.title.toLowerCase().includes(searchBar.toLowerCase())
+  );
   return (
     <>
       <Header />
       <SectionLink />
-
-
-      {sofasPopularesData.map((product) => (
-         <ProductCard key={product.productId} {...product} />
+      <SearchBar  value={searchBar} onChange={setSearchBar}/>
+      {filteredFurnitures.map((product) => (
+        <ProductCard key={product.productId} {...product} />
       ))}
-
       <ScrollUp />
       <Footer />
     </>
