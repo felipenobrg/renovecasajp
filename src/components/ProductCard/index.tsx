22b1914b0a7061b33e564bdca491ebe19b812682
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-09-21 13:36:45 -0300

    feat: changing location to useLocation

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,18 +1,7 @@
 import { WhatsappLogo } from "phosphor-react";
-import {
-  CardContainer,
-  CardContent,
-  TitleCard,
-  Price,
-  MoneySign,
-  OldPrice,
-  ButtonCard,
-  WornOut,
-  Click,
-} from "./styles";
+import { CardContainer, CardContent, TitleCard, Price, MoneySign, OldPrice, ButtonCard, WornOut, Click } from "./styles";
 import queryString from "query-string";
-import { NavLink } from "react-router-dom";
-
+import { NavLink, useLocation } from "react-router-dom"; 
 
 export interface ProductCardProps {
   imgSrc: string;
@@ -23,7 +12,7 @@ export interface ProductCardProps {
   textForWhatsApp: string;
   productId: number;
   description?: string;
-  key: number
+  key: number;
 }
 
 export function ProductCard({
@@ -35,6 +24,8 @@ export function ProductCard({
   wornOut,
   productId,
 }: ProductCardProps) {
+  const location = useLocation(); 
+
   const handleWhatsAppClick = () => {
     const url = `https://api.whatsapp.com/send?${queryString.stringify({
       phone: "5583987663399",
@@ -43,41 +34,33 @@ export function ProductCard({
     window.open(url, "_blank");
   };
 
-  let linkDestination 
-  
-  switch (true) {
-    case location.pathname.includes("/sofaretro"):
-      linkDestination = `/product/${productId}`;
-      break;
-    case location.pathname.includes("/sofaretratil"):
-      linkDestination = `/product2/${productId}`;
-      break;
-    case location.pathname.includes("/sofaspopulares"):
-      linkDestination = `/product3/${productId}`;
-      break;
-    case location.pathname.includes("/poltronas"):
-      linkDestination = `/product4/${productId}`;
-      break;
-    case location.pathname.includes("/camas"):
-      linkDestination = `/product5/${productId}`;
-      break;
-    case location.pathname.includes("/produtosvime"):
-      linkDestination = `/product6/${productId}`;
-      break;
-    case location.pathname.includes("/moveis"):
-      linkDestination = `/product7/${productId}`;
-      break;
-    case location.pathname.includes("/utilidades"):
-      linkDestination = `/product8/${productId}`;
-      break;
-    default:
-      linkDestination = `/product/${productId}`;
-  }
+  const getLinkDestination = () => {
+    switch (true) {
+      case location.pathname.includes("/sofaretro"):
+        return `/product/${productId}`;
+      case location.pathname.includes("/sofaretratil"):
+        return `/product2/${productId}`;
+      case location.pathname.includes("/sofaspopulares"):
+        return `/product3/${productId}`;
+      case location.pathname.includes("/poltronas"):
+        return `/product4/${productId}`;
+      case location.pathname.includes("/camas"):
+        return `/product5/${productId}`;
+      case location.pathname.includes("/produtosvime"):
+        return `/product6/${productId}`;
+      case location.pathname.includes("/moveis"):
+        return `/product7/${productId}`;
+      case location.pathname.includes("/utilidades"):
+        return `/product8/${productId}`;
+      default:
+        return `/product/${productId}`;
+    }
+  };
 
-  return ( 
+  return (
     <CardContainer>
       <CardContent>
-        <NavLink to={linkDestination}>
+        <NavLink to={getLinkDestination()}>
           <img loading="lazy" src={imgSrc} alt="" />
           <Click>Clique para saber mais</Click>
           {wornOut && <WornOut>{wornOut}</WornOut>}
