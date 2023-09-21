c7a5884cb9ea84006dd7dd7909eeded8f8646a82
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-09-21 13:20:30 -0300

    feat: adding new products in moveis pages

--- src/data/Moveis.ts
+++ src/data/Moveis.ts
@@ -1,6 +1,8 @@
 import ArmarioPreto from '../assets/Moveis/ArmarioDeCozinhaPreto.jpg';
 import ArmarioBranco from '../assets/Moveis/ArmarioDeCozinhaBranco.jpg';
 import ArmarioMarrom from '../assets/Moveis/ArmarioDeCozinhaMarrom.jpg';
+import MesaDeJantarPreta from "../assets/Moveis/MesaDeJantarPreta.jpg"
+import MesaDeJantarBranca from "../assets/Moveis/MesaDeJantarBranca.jpg"
 import PainelDeTv46Polegadas from '../assets/Moveis/PainelParaTv.jpg';
 import PainelDeTv55Polegadas from '../assets/Moveis/PainelParaTv55.jpg';
 
@@ -8,6 +10,24 @@ import PainelDeTv55Polegadas from '../assets/Moveis/PainelParaTv55.jpg';
   export const moveisData = [
     {
       productId: 1,
+      imgSrc: MesaDeJantarPreta,
+      title: "Mesa de Jantar Preta 70x70",
+      price: "499,99",
+      oldPrice: "559,99",
+      cardValue: "48,99",
+      textForWhatsApp: "Olá. Gostaria de comprar Mesa de Jantar Preta",
+    },
+    {
+      productId: 2,
+      imgSrc: MesaDeJantarBranca,
+      title: "Mesa de Jantar Branca 70x70",
+      price: "749,99",
+      oldPrice: "899,99",
+      cardValue: "48,99",
+      textForWhatsApp: "Olá. Gostaria de comprar Armário de Cozinha Preto e Branco",
+    },
+    {
+      productId: 3,
       imgSrc: ArmarioPreto,
       title: "Armário de Cozinha Preto",
       price: "749,99",
@@ -16,7 +36,7 @@ import PainelDeTv55Polegadas from '../assets/Moveis/PainelParaTv55.jpg';
       textForWhatsApp: "Olá. Gostaria de comprar Armário de Cozinha Preto e Branco",
     },
     {
-      productId: 2,
+      productId: 4,
       imgSrc: ArmarioBranco,
       title: "Armário de Cozinha Preto e Branco",
       price: "749,99",
@@ -25,7 +45,7 @@ import PainelDeTv55Polegadas from '../assets/Moveis/PainelParaTv55.jpg';
       textForWhatsApp: "Olá. Gostaria de comprar Armário de Cozinha Preto e Branco",
     },
     {
-      productId: 3,
+      productId: 5,
       imgSrc: ArmarioMarrom,
       title: "Armário de Cozinha Marrom",
       price: "749,99",
@@ -34,9 +54,8 @@ import PainelDeTv55Polegadas from '../assets/Moveis/PainelParaTv55.jpg';
       textForWhatsApp: "Olá. Gostaria de comprar Armário de Cozinha Marrom",
     },
     {
-      productId: 4,
+      productId: 6,
       imgSrc: PainelDeTv46Polegadas,
-      wornOut: "ESGOTADO",
       title: "Painel de TV 46 Polegadas",
       price: "399,99",
       oldPrice: "599,99",
@@ -44,7 +63,7 @@ import PainelDeTv55Polegadas from '../assets/Moveis/PainelParaTv55.jpg';
       textForWhatsApp: "Olá. Gostaria de comprar Painel de TV 46 Polegadas",
     },
     {
-      productId: 5,
+      productId: 7,
       imgSrc: PainelDeTv55Polegadas,
       title: "Painel de TV 55 Polegadas",
       price: "749,99",
