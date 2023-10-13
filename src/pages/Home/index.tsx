656c4597fafc085735fd3fce2ad47b3fc646878b
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-10-13 20:03:27 -0300

    feat: removing spinner

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { AboutDelivery } from "../../components/AboutDelivery";
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
@@ -8,23 +7,9 @@ import { CardPhotos } from "./Components/CardPhotos";
 import { Carousel } from "./Components/Carousel";
 import { SectionAbout } from "./Components/SectionAbout";
 import { WhoWeAre } from "./Components/WhoWeAre";
-import { LoadingSpinner } from "../../components/LoadingSpinner";
 
 export function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const delay = setTimeout(() => {
-      setIsLoading(false);
-      clearTimeout(delay);
-    }, 2000);
-  }, []);
-
   return (
-    <>
-      {isLoading ? (
-        <LoadingSpinner />
-      ) : (
         <>
           <Header />
           <AboutDelivery />
@@ -37,6 +22,4 @@ export function Home() {
           <Footer />
         </>
       )}
-    </>
-  );
-}
+
