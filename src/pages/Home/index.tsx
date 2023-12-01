39eab00320774e00495bd62da9808c98c6966c2d
Felipe Nóbrega <felipenobrega2012@gmail.com>
2023-12-01 14:55:24 -0300

    feat: removing carousel

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,6 @@ import { Header } from "../../components/Header";
 import { ScrollUp } from "../../components/ScrollUp";
 import { SectionLink } from "../../components/SectionLink";
 import { CardPhotos } from "./Components/CardPhotos";
-import { Carousel } from "./Components/Carousel";
 import { Promotions } from "./Components/Promotions";
 import { SectionAbout } from "./Components/SectionAbout";
 import { WhoWeAre } from "./Components/WhoWeAre";
@@ -15,7 +14,6 @@ export function Home() {
           <Header />
           <AboutDelivery />
           <SectionLink />
-          <Carousel />
           <CardPhotos />
           <SectionAbout />
           <Promotions />
